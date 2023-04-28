399fc066ba94106a2a3324757ca68b020bdf3909
Aluno <Aluno@ISTECPC-A8-005>
2023-04-28 18:54:11 +0100

    Alterações Novas

--- DAS.php
+++ DAS.php
@@ -6,16 +6,4 @@ echo NL;
 $bravo = array(3,4,5,6);
 print_r($bravo);
 
-$charlie =[2,3,4,5,6];
-echo NL;
-foreach($charlie as $pass)echo $pass . ":";
-echo NL;
-$charlie[]=1000;
-foreach($charlie as $pass)echo $pass . ":";
-echo NL;
-$charlie[2]='cota';
-foreach($charlie as $pass)echo $pass . ":";
-echo NL;
-unset($charlie[2]);
-foreach($charlie as $pass)echo $pass . ":";
 ?>
